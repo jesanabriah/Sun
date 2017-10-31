51e888b0c1c7729957dd9fe6c8f190773532ecb1
Jorge Sanabria <jesanabriah@unal.edu.co>
2017-10-31 02:38:56 -0500

    Organizando todas las funciones en la libreria

--- lib/libsun.py
+++ lib/libsun.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 '''
 Created on 27/08/2017
 
@@ -31,7 +32,29 @@ from os import listdir
 from scipy import misc
 from scipy import ndimage
 import time
+import pickle
 
+# Valores de configuracion del programa:
+X, Y, YX, T = 1, 0, 0, 1
+
+# Valor predeterminado para el error
+XERR, DX_MIN, YERR = 6, 1, 2
+
+# Lee objetos desde archivo
+def load_data():
+    try:
+        with open("output/datos.dat") as f:
+            datos = pickle.load(f)
+    except:
+        datos = {}
+    return datos
+
+# Guarda objetos en archivo
+def save_data(data):
+    with open("output/datos.dat", "wb") as f:
+        pickle.dump(data, f)
+
+# Crea lista con nombres de archivos
 def ls(ruta='.'):
     '''
     Muestra la lista de archivos del directorio especificado.
@@ -40,6 +63,163 @@ def ls(ruta='.'):
     Si no especifica valor, muestra la lista de archivos del directorio actual.
     '''
     return listdir(ruta)
+
+def getCMFromImage(archivo = "", factor = 0.5):
+    '''
+    A partir de archivo procesa una imagen del Sol,
+    detectando los centros de masa con un factor
+    por encima del valor medio y por debajo del maximo.
+
+    @param archivo: la ruta del archivo a procesar
+
+    @param factor: Desde 0 para valor promedio, hasta 1 para valor maximo
+
+    @return: retorna centros de masa para la imagen
+    '''
+
+    # Lee la imagen del disco duro
+    img = misc.imread ("img/" + archivo, 1)
+
+    # Calcula el valor maximo de intensidad para un pixel e invierte todos los valores, de modo que ahora oscuridad = max y luz = 0
+    maximum = ndimage.maximum(img)
+    img = maximum - img
+
+    # calcula para los nuevos datos maximo y valor promedio
+    median = ndimage.median(img)
+    maximum = ndimage.maximum(img)
+
+    # Crea filtro con un criterio muy relativo :P
+    filtro = img > median + (maximum - median) * factor
+    img = img * filtro
+
+    # Calcula los centros de masa con un maximo de NUM posibles manchas
+    lbl, num = ndimage.label(img)
+
+    # centros de masa
+    #center_of_mass = ndimage.measurements.center_of_mass(img, lbl, range(2, num + 1))
+    # El primer valor es el centro del Sol
+    center_of_mass = ndimage.measurements.center_of_mass(img, lbl, range(1, num + 1))
+
+    # lee el tiempo de toma de la imagen
+    arch = archivo.split("_", 2)
+    tiempo = time.strptime(arch[0] + arch[1], "%Y%m%d%H%M%S")
+
+    # return centros de masa y tiempo de foto
+    return center_of_mass, time.mktime(tiempo)
+
+def resetMosaico(res):
+    'Reset images output/mosaico_*.png with lib/img/sun_*.png'
+
+    img = misc.imread ("lib/img/sun_" + res + ".png")
+    misc.imsave("output/mosaico_" + res + ".png", img)
+
+def getCenterofMassesofImages():
+    #lee la lista de archivos
+    archivos = ls("img/")
+
+    # Los parametros recomendados son para imagenes de 4096
+    # Procesa imagenes tipo *.jpg
+    centers_of_masses = {}
+
+    index = 0
+    for archivo in archivos:
+        if archivo[-4:] == ".jpg":
+            centers_of_masses[index] = getCMFromImage(archivo)
+            print "Procesada: " + archivo
+            #print center_of_mass[index]
+            index = index + 1
+
+    return centers_of_masses
+
+
+def getKey2(item):
+    return item[2]
+
+def getKey3(item):
+    return item[3]
+
+def get_ve_com(point, next_points):
+
+    res = []
+
+    for i in range(len(next_points)):
+        #revisando xpos
+        if next_points[i][X] > point[X] + DX_MIN: # El +DX_MIN asegura que la mancha si se mueva
+            if next_points[i][X] < point[X] + XERR:
+                #revisando ypos
+                if next_points[i][Y] > point[Y] - YERR:
+                    if next_points[i][Y] < point[Y] + YERR:
+                        # y, x, dy, dx, index of next point
+                        val = next_points[i][0], next_points[i][1], abs(next_points[i][Y] - point[Y]), abs(next_points[i][X] - point[X]), i
+                        res.append(val)
+
+    #primero ordenar
+    res.sort(cmp=None, key=getKey3, reverse=False)
+    res.sort(cmp=None, key=getKey2, reverse=False)
+
+    try:
+        return res[0]
+    except IndexError:
+        return ()
+
+def getCenter(comsoi):
+
+    promedio_x = 0
+    promedio_y = 0
+
+    # suma todos los valores de los centros de las imagenes
+    for i in comsoi:
+        promedio_x = promedio_x + comsoi[i][YX][0][X] #primer valor es el centro de la imagen
+        promedio_y = promedio_y + comsoi[i][YX][0][Y] #primer valor es el centro de la imagen
+
+    # calcula la cantidad de imagenes y divide para calcular el promedio
+    tam = len(comsoi)
+    promedio_x = promedio_x / tam
+    promedio_y = promedio_y / tam
+
+    return promedio_y, promedio_x
+
+def get_ve_comsoi(coms, next_coms):
+
+    ve_coms = []
+
+    for i in range(len(coms[YX])):
+        ve_coms.append(get_ve_com(coms[YX][i], next_coms[YX]))
+
+    temp = next_coms[T], abs(next_coms[T] - coms[T])
+
+    return ve_coms, temp
+
+def get_ve_comsois(comsois):
+    ve_comsois = {}
+    #Calcula valores esperados para cada imagen
+    for i in range(len(comsois) - 1):
+        ve_comsois[i] = get_ve_comsoi(comsois[i], comsois[i + 1])
+
+    return ve_comsois
+
+def saveMosaico(ve_comsoi, res):
+    #Dibujar mosaico
+    print "Dibujando seguimiento de manchas en: output/mosaico_" + res + ".png"
+    # mosaico_~.png en blanco
+    resetMosaico(res)
+    # Lee archivo
+    mosaico = misc.imread ("output/mosaico_" + res + ".png", 1)
+    # Dibuja linea con 3 puntos en (y, x), (y, x + 1), (y, x + 2)
+    for i in ve_comsoi:
+        for ve_com in ve_comsoi[i][YX]:
+            try:
+                y = int(ve_com[0])
+                x = int(ve_com[1])
+                mosaico[y, x] = 0
+                mosaico[y, x + 1] = 0
+                mosaico[y, x + 2] = 0
+            except IndexError:
+                pass
+            except TypeError:
+                pass
+    # Guarda imagen procesada
+    misc.imsave("output/mosaico_" + res + ".png", mosaico)
 
 def procesar_imagen(archivo = "", factor = 0.5, file = "white"):
     '''
@@ -95,21 +275,3 @@ def procesar_imagen(archivo = "", factor = 0.5, file = "white"):
 
     misc.imsave("output/img/" + archivo, sun)
     misc.imsave("output/mosaico_" + arch[2] + ".png", mosaico)
-
-def resetMosaico():
-    'Reset images output/mosaico_*.png with lib/img/sun_*.png'
-
-    img = misc.imread ("lib/img/sun_512.png")
-    misc.imsave("output/mosaico_512.png", img)
-
-    img = misc.imread ("lib/img/sun_1024.png")
-    misc.imsave("output/mosaico_1024.png", img)
-
-    img = misc.imread ("lib/img/sun_2048.png")
-    misc.imsave("output/mosaico_2048.png", img)
-
-    img = misc.imread ("lib/img/sun_3072.png")
-    misc.imsave("output/mosaico_3072.png", img)
-
-    img = misc.imread ("lib/img/sun_4096.png")
-    misc.imsave("output/mosaico_4096.png", img)
--- sun.py
+++ sun.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 '''
 Created on 26/08/2017
 
@@ -26,247 +27,56 @@ Created on 26/08/2017
 
     @organization: Universidad Nacional de Colombia
 '''
-from scipy.ndimage.measurements import center_of_mass
-from os import listdir
-from scipy import misc
-from scipy import ndimage
-import time
-import pickle
+
+from lib import libsun as sun
 import sys
 
-# Valores de configuracion del programa:
-X = 1
-Y = 0
-YX = 0
-T = 1
-# Valor predeterminado para el error en x
-XERR = 6
-DX_MIN = 0.7
-# Valor predeterminado para el error en y
-YERR = 2
+# Resolucion
+RESOLUCION = ""
 
+# Centro de las imagenes del sol
 center = 0, 0
 
-RESOLUCION = "4096"
+# Diccionario para guardar los valores de cada mancha
+comsois = {}
 
-def load_data():
-    try:
-        with open("output/datos.dat") as f:
-            datos = pickle.load(f)
-    except:
-        datos = {}
-    return datos
+# Diccionario para guardar los siguientes valores esperados para cada mancha
+ve_comsois = {}
 
-def save_data(data):
-    with open("output/datos.dat", "wb") as f:
-        pickle.dump(data, f)
+def mainSun():
 
-def ls(ruta='.'):
-    '''
-    Muestra la lista de archivos del directorio especificado.
+    # Haya el centro del sol en pixeles en base a un promedio del centro de todas las imagenes
+    center = sun.getCenter(comsois)
 
-    @return: Lista de nombres de archivos del directorio especificado.
-    Si no especifica valor, muestra la lista de archivos del directorio actual.
-    '''
-    return listdir(ruta)
+    # Calcula valores esperados para las manchas de todas las imagenes
+    ve_comsois = sun.get_ve_comsois(comsois)
 
-def getCMFromImage(archivo = "", factor = 0.5):
-    '''
-    A partir de archivo procesa una imagen del Sol,
-    detectando los centros de masa con un factor
-    por encima del valor medio y por debajo del maximo.
+    # Guarda grafico de seguimiento de machas en archivo
+    sun.saveMosaico(ve_comsois, RESOLUCION)
 
-    @param archivo: la ruta del archivo a procesar
+    # Procedimiento exitoso
+    print "Procedimiento exitoso"
 
-    @param factor: Desde 0 para valor promedio, hasta 1 para valor maximo
-
-    @return: retorna centros de masa para la imagen
-    '''
-
-    # Lee la imagen del disco duro
-    img = misc.imread ("img/" + archivo, 1)
-
-    # Calcula el valor maximo de intensidad para un pixel e invierte todos los valores, de modo que ahora oscuridad = max y luz = 0
-    maximum = ndimage.maximum(img)
-    img = maximum - img
-
-    # calcula para los nuevos datos maximo y valor promedio
-    median = ndimage.median(img)
-    maximum = ndimage.maximum(img)
-
-    # Crea filtro con un criterio muy relativo :P
-    filtro = img > median + (maximum - median) * factor
-    img = img * filtro
-
-    # Calcula los centros de masa con un maximo de NUM posibles manchas
-    lbl, num = ndimage.label(img)
-
-    # centros de masa
-    #center_of_mass = ndimage.measurements.center_of_mass(img, lbl, range(2, num + 1))
-    center_of_mass = ndimage.measurements.center_of_mass(img, lbl, range(1, num + 1))
-
-    # lee el tiempo de toma de la imagen
-    arch = archivo.split("_", 2)
-    tiempo = time.strptime(arch[0] + arch[1], "%Y%m%d%H%M%S")
-
-    # return centros de masa y tiempo de foto
-    return center_of_mass, time.mktime(tiempo)
-
-def resetMosaico():
-    'Reset images output/mosaico_*.png with lib/img/sun_*.png'
-
-    img = misc.imread ("lib/img/sun_512.png")
-    misc.imsave("output/mosaico_512.png", img)
-
-    img = misc.imread ("lib/img/sun_1024.png")
-    misc.imsave("output/mosaico_1024.png", img)
-
-    img = misc.imread ("lib/img/sun_2048.png")
-    misc.imsave("output/mosaico_2048.png", img)
-
-    img = misc.imread ("lib/img/sun_3072.png")
-    misc.imsave("output/mosaico_3072.png", img)
-
-    img = misc.imread ("lib/img/sun_4096.png")
-    misc.imsave("output/mosaico_4096.png", img)
-
-# crea lista con nombres de archivos
-
-
-def getCM_X(center_of_mass, n):
-    return center_of_mass[0][n][1]
-
-def getCM_Y(center_of_mass, n):
-    return center_of_mass[0][n][0]
-
-def getCM_T(center_of_mass, n):
-    return center_of_mass[1]
-
-def getCM_TXY(center_of_mass, n):
-    return center_of_mass[1], center_of_mass[0][n][1], center_of_mass[0][n][0]
-
-
-def getCenterofMassesofImage():
-    #lee la lista de archivos
-    archivos = ls("img/")
-
-    # mosaico_~.png en blanco
-    #resetMosaico()
-
-    # Los parametros recomendados son para imagenes de 4096
-    # Procesa imagenes tipo *.jpg
-    centers_of_masses = {}
-
-    index = 0
-    for archivo in archivos:
-        if archivo[-4:] == ".jpg":
-            centers_of_masses[index] = getCMFromImage(archivo)
-            print "Procesada: " + archivo
-            #print center_of_mass[index]
-            index = index + 1
-
-    return centers_of_masses
-
-
-def getKey2(item):
-    return item[2]
-
-def getKey3(item):
-    return item[3]
-
-def get_ve_com(point, next_points):
-
-    res = []
-
-    for i in range(len(next_points)):
-        #revisando xpos
-        if next_points[i][X] > point[X] + DX_MIN: # El +DX_MIN asegura que la mancha si se mueva
-            if next_points[i][X] < point[X] + XERR:
-                #revisando ypos
-                if next_points[i][Y] > point[Y] - YERR:
-                    if next_points[i][Y] < point[Y] + YERR:
-                        # y, x, dy, dx, index of next point
-                        val = next_points[i][0], next_points[i][1], abs(next_points[i][Y] - point[Y]), abs(next_points[i][X] - point[X]), i
-                        res.append(val)
-
-    #primero ordenar
-    res.sort(cmp=None, key=getKey3, reverse=False)
-    res.sort(cmp=None, key=getKey2, reverse=False)
-
-    try:
-        return res[0]
-    except IndexError:
-        return ()
-
-def getCenter(comsoi):
-
-    promedio_x = 0
-    promedio_y = 0
-
-    # suma todos los valores de los centros de las imagenes
-    for i in comsoi:
-        promedio_x = promedio_x + comsoi[i][YX][0][X] #primer valor es el centro de la imagen
-        promedio_y = promedio_y + comsoi[i][YX][0][Y] #primer valor es el centro de la imagen
-
-    # calcula la cantidad de imagenes y divide para calcular el promedio
-    tam = len(comsoi)
-    promedio_x = promedio_x / tam
-    promedio_y = promedio_y / tam
-
-    return promedio_y, promedio_x
-
-def get_ve_comsoi(coms, next_coms):
-
-    ve_coms = []
-
-    for i in range(len(coms[YX])):
-        ve_coms.append(get_ve_com(coms[YX][i], next_coms[YX]))
-
-    temp = next_coms[T], abs(next_coms[T] - coms[T])
-    #ve_coms.append(temp)
-
-    return ve_coms, temp
-
-#inicia el procedo dejando todo en blanco
-resetMosaico()
-
+# Get center of masses of images and resolution
 # Si se especifica en la linea de comandos entonces no procesara las imagenes
-if len(sys.argv) > 1 and sys.argv[1] == "-f":
-    comsoi = load_data()
+if len(sys.argv) == 2:
+    if sys.argv[1] == "-l":
+        comsois, RESOLUCION = sun.load_data()
+        mainSun()
+    elif sys.argv[1] == "512" or sys.argv[1] == "1024" or sys.argv[1] == "2048" or sys.argv[1] == "3072" or sys.argv[1] == "4096":
+        RESOLUCION = sys.argv[1]
+        comsois = sun.getCenterofMassesofImages()
+        datos = comsois, RESOLUCION
+        sun.save_data(datos)
+        mainSun()
+    else:
+        print "Argumento invalido. Pruebe:"
+        print ""
+        print "python sun.py [[RESOLUCION] [-l]]"
+        print "Use opción '-l' para cargar datos ya procesados."
 else:
-    comsoi = getCenterofMassesofImage()
-    save_data(comsoi)
+    print "Numero de argumentos invalidos. Pruebe:"
+    print ""
+    print "python sun.py [[RESOLUCION] [-l]]"
+    print "Use opción '-l' para cargar datos ya procesados."
 
-center = getCenter(comsoi)
-
-ve_comsoi = {}
-
-for i in range(len(comsoi) - 1):
-    ve_comsoi[i] = get_ve_comsoi(comsoi[i], comsoi[i + 1])
-
-#Dibujar mosaico
-print "Dibujando seguimiento de manchas en: output/mosaico_" + RESOLUCION + ".png"
-
-mosaico = misc.imread ("output/mosaico_" + RESOLUCION + ".png", 1)
-
-for i in ve_comsoi:
-    for ve_com in ve_comsoi[i][YX]:
-        try:
-            #print ve_com
-            y = int(ve_com[0])
-            x = int(ve_com[1])
-            mosaico[y, x] = 0
-            mosaico[y, x + 1] = 0
-            mosaico[y, x + 2] = 0
-            #print y, x
-        except IndexError:
-            pass
-        except TypeError:
-            pass
-
-misc.imsave("output/mosaico_" + RESOLUCION + ".png", mosaico)
-
-print "Procedimiento exitoso"
-#print comsoi
-#print ""
-#print ve_comsoi
